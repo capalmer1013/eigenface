4c9113c58a876578667ab3e1ecf7120bb1cb07f9
Chris Palmer <capalmer1013@gmail.com>
2020-01-18 00:46:07 -0500

    added eigenface structure

---
+++ pygenface/structures.py
@@ -0,0 +1,24 @@
+class Eigenface(object):
+    def __init__(self):
+        pass
+
+    def add(self, face):
+        pass
+
+    def addMultiple(self, listOfFaces):
+        pass
+
+    def addFile(self, filename):
+        pass
+
+    def addMultipleFiles(self, listOfFilenames):
+        pass
+
+    def load(self, filename):
+        pass
+
+    def save(self, filename):
+        pass
+
+    def compare(self, filename):
+        pass
